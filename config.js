4243581c43dc324ff1eded0183cb72ae092f786a
Mao Chenyu <chenyumao815@outlook.com>
2026-02-03 19:53:52 +0800

    feat: 重构音乐播放器项目结构并添加本地音乐
    将音乐资源移至music目录，更新相关文件引用路径
    添加本地音乐文件及配置，修改.gitignore规则
    更新页面标题为ZSXMusic，调整资源引用路径

---
+++ config.js
@@ -0,0 +1,12 @@
+// var userId = "8668419170";
+// var userServer = "tencent";
+// var userType = "playlist";
+var localMusic = [{
+    name: '一句话形容不了终极笔记',
+    artist: '应有棠 叶落梦中 御A桑 灬阿楚灬 绯言 小山xl 霄镁 烫不直的自然卷 堇墨安歌 天罗',
+    url: '/music/一句话形容不了终极笔记.mp3',
+    cover: '/music/一句话形容不了终极笔记.png',
+    lrc: '/music/一句话形容不了终极笔记.lrc'
+}
+];
+// var remoteMusic = "./musiclist.json"

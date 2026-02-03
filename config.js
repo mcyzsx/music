b93e9a02e734fd750c7c454cff3e24d9df7dbfcd
Mao Chenyu <chenyumao815@outlook.com>
2026-02-03 19:58:26 +0800

    fix: 修正音乐封面图片格式从png改为jpg

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ var localMusic = [{
     name: '一句话形容不了终极笔记',
     artist: '应有棠 叶落梦中 御A桑 灬阿楚灬 绯言 小山xl 霄镁 烫不直的自然卷 堇墨安歌 天罗',
     url: '/music/一句话形容不了终极笔记.mp3',
-    cover: '/music/一句话形容不了终极笔记.png',
+    cover: '/music/一句话形容不了终极笔记.jpg',
     lrc: '/music/一句话形容不了终极笔记.lrc'
 }
 ];
